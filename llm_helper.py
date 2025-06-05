fd31c31a0f59377ea3e65905a74843ed1082cb21
Ankush Dhankhar <ankushdhankhar3002@gmail.com>
2025-06-05 10:08:24 +0530

    final

--- llm_helper.py
+++ llm_helper.py
@@ -1,12 +1,12 @@
-from dotenv import load_dotenv
+import streamlit as st
 from langchain_groq import ChatGroq
 import os
 
 from pydantic.v1.schema import model_type_schema
 
-load_dotenv()
+api_key = st.secrets["OPENAI_API_KEY"]
 
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name = "llama-3.3-70b-versatile")
+llm = ChatGroq(groq_api_key=api_key, model_name = "llama-3.3-70b-versatile")
 
 if __name__ == "__main__" :
     response = llm.invoke("what is main ingridients in samosa ")
