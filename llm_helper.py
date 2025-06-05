b130183dfdc23b7f5052b04d5ed9a19a7f92e5e3
Ankush Dhankhar <ankushdhankhar3002@gmail.com>
2025-06-05 09:48:38 +0530

    tr

--- llm_helper.py
+++ llm_helper.py
@@ -6,8 +6,8 @@ from pydantic.v1.schema import model_type_schema
 
 load_dotenv()
 
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name = "llama3-8b-8192")
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name = "llama-3.3-70b-versatile")
 
 if __name__ == "__main__" :
     response = llm.invoke("what is main ingridients in samosa ")
-    print(response.content)
+    # print(response.content)
